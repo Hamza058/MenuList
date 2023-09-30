2d6b3ae9ab663dd3698c556007d48789fd5fc503
hamza kılıç <hamza2358@hotmail.com>
2023-09-30 14:06:14 +0300

    updated entity

---
+++ Entity/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public Category Categories { get; set; }
+    }
+}
