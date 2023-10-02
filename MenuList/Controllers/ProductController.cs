7b85ff64898e39edfde6c18dbc42f2597c14574f
hamza kılıç <hamza2358@hotmail.com>
2023-10-02 18:07:31 +0300

    added product controller and product index page

---
+++ MenuList/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuList.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager pm = new ProductManager(new EFProductDal());
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Json(pm.TGetList());
+		}
+
+		[HttpPost]
+		public IActionResult Add(Product product)
+		{
+            pm.TAdd(product);
+			return Json(new { IsSuccess = "true" });
+		}
+	}
+}
